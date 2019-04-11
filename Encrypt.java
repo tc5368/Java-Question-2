e0dfb727f7b90f0a687c5424d4ff2c17af92d73b
Tom <tc536890@gmail.com>
2019-04-11 21:06:15 +0100

    Finished just needs commenting

--- Encrypt.java
+++ Encrypt.java
@@ -4,33 +4,41 @@ import java.util.*;
 public class Encrypt{
 
 	public static void main(String [] args){
+
 		if (args.length != 1){
 			System.err.println("One argument expected!");
 			System.exit(1);
 		}
-	try{
-		FileReader fr = new FileReader(args[0]);
-		int i;
-		int newAscii = 0;
-		StringBuilder sb = new StringBuilder(""); 
+		
+		String filename = args[0];
+		try{
+			FileInputStream fileIs = new FileInputStream(filename);
+			ObjectInputStream is = new ObjectInputStream(fileIs);
+			String readIn = is.readUTF();
+			is.close();
 
-		while((i =fr.read()) != -1){
-			if (i <=122  && i >=98){
-				newAscii = i -1;
-			}
-			else if ((int) i == 97){
-				newAscii = 122;
-			}
-			else{
-				newAscii = i;
-			}
-			char outText = (char) newAscii;
-			sb.append(outText);
-			}
-		System.out.println(sb);
-	}
-	catch(IOException e){
-		System.out.println("No Such File");
-	}
+			char[] characters = readIn.toCharArray();
+			StringBuilder sb = new StringBuilder("");
+			for(char i : characters){
+				int newAscii = (int) i;
+				if (i <=122  && i >=98){
+					newAscii = i -1;
+				}
+				else if ((int) i == 97){
+					newAscii = 122;
+				}
+				else{
+					newAscii = i;
+				}
+				char outText = (char) newAscii;
+				sb.append(outText);
+				}
+			System.out.println(sb);
+
+		}catch(FileNotFoundException e){
+			System.out.println("File not found");
+		}catch(IOException e){
+			System.out.println("IO error");
+		}
 	}
 }
